700e1ff5d9ce9cb3a4b1f596f99fe45ce5f00832
leandrocgsi <leandrocgsi@gmail.com>
2025-07-14 11:33:51 -0300

    Preparing Microservices Stack to Run in Kuberntes Cluster

---
+++ 19_Kubernetes/book-service/src/main/java/br/com/erudio/proxy/ExchangeProxy.java
@@ -0,0 +1,19 @@
+package br.com.erudio.proxy;
+
+import br.com.erudio.dto.Exchange;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.math.BigDecimal;
+
+@FeignClient(name = "exchange-service", url = "${EXCHANGE_SERVICE_HOST:http://localhost}:8000")
+// @FeignClient(name = "exchange-service")
+public interface ExchangeProxy {
+
+    @GetMapping(value = "/exchange-service/{amount}/{from}/{to}")
+    public Exchange getExchange(
+            @PathVariable("amount") Double amount,
+            @PathVariable("from") String from,
+            @PathVariable("to") String to);
+}
