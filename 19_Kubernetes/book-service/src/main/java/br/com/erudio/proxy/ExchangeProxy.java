c26bdad90547b9bf5c74f3dde4cb1da8dcb7d8fe
leandrocgsi <leandrocgsi@gmail.com>
2025-07-17 09:39:25 -0300

    Preparing Microservices Stack to Run in Kuberntes Cluster

--- 19_Kubernetes/book-service/src/main/java/br/com/erudio/proxy/ExchangeProxy.java
+++ 19_Kubernetes/book-service/src/main/java/br/com/erudio/proxy/ExchangeProxy.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.math.BigDecimal;
 
-@FeignClient(name = "exchange-service", url = "${EXCHANGE_SERVICE_HOST:http://host.docker.internal}:8000")
+@FeignClient(name = "exchange-service", url = "${EXCHANGE_SERVICE_URI:http://host.docker.internal}:8000")
+//@FeignClient(name = "exchange-service", url = "${EXCHANGE_SERVICE_SERVICE_HOST:http://host.docker.internal}:8000")
 //@FeignClient(name = "exchange-service")
 public interface ExchangeProxy {
 
